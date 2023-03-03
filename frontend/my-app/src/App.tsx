9c2e3e6408dae069cd17252683326f6561ae96c0
Pallavi Jain <107476738+pallavijainy@users.noreply.github.com>
2023-03-04 01:29:38 +0530

    add

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
@@ -24,6 +24,31 @@ function App() {
     dealerWin = "Dealer Wins!",
     tie = "Tie!",
   }
+
+  const data = JSON.parse(JSON.stringify(jsonData.cards));
+  const [deck, setDeck]: any[] = useState(data);
+  // console.log(deck);
+  //user
+  const [userCards, setUserCards]: any[] = useState([]);
+  const [userScore, setUserScore] = useState(0);
+  const [userCount, setUserCount] = useState(0);
+  //dealer
+  const [dealerCards, setDealerCards]: any[] = useState([]);
+  const [dealerScore, setDealerScore] = useState(0);
+  const [dealerCount, setDealerCount] = useState(0);
+  //balance
+  const [balance, setBalance] = useState(1000);
+  const [bet, setBet] = useState(0);
+
+  const [gameState, setGameState] = useState(GameState.bet);
+  const [message, setMessage] = useState(Message.bet);
+
+  const [buttonState, setButtonState] = useState({
+    hitDisabled: false,
+    standDisabled: false,
+    resetDisabled: true,
+  });
+
   return <div className="App"></div>;
 }
 
