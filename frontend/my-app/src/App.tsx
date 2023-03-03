f1aaf3c4632bd6f74bcd28d2613dadeddaae2148
Pallavi Jain <107476738+pallavijainy@users.noreply.github.com>
2023-03-03 23:10:14 +0530

    add enum

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -3,6 +3,27 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  enum GameState {
+    bet,
+    init,
+    userTurn,
+    dealerTurn,
+  }
+
+  enum Deal {
+    user,
+    dealer,
+    hidden,
+  }
+
+  enum Message {
+    bet = "Place a Bet!",
+    hitStand = "Hit or Stand?",
+    bust = "Bust!",
+    userWin = "You Win!",
+    dealerWin = "Dealer Wins!",
+    tie = "Tie!",
+  }
   return <div className="App"></div>;
 }
 
